10baf8f3290d07a052d7eafdcf32b2c7dca6d6fb
Alin Ungureanu <alin.watsel@yahoo.com>
2026-04-10 18:30:16 +0100

    fix window not defined error for web build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,31 @@
-import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native';
 import { createClient } from '@supabase/supabase-js';
+import type { SupportedStorage } from '@supabase/supabase-js';
+
+if (Platform.OS !== 'web') {
+  require('react-native-url-polyfill/auto');
+}
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+const localStorageAdapter: SupportedStorage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const getStorage = (): SupportedStorage => {
+  if (Platform.OS === 'web') {
+    return localStorageAdapter;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  return require('@react-native-async-storage/async-storage').default;
+};
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: getStorage(),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
